2ae21350a033205871c91e88d0967feede71c04a
reema-harnekar <reema.harnekar98@gmail.com>
2018-08-08 21:48:26 +0530

    4th commit

--- js/app.js
+++ js/app.js
@@ -1,4 +1,24 @@
-var elements = document.getElementsByTagName('h1');
-console.log(elements[0].textContent);
-var elements = document.getElementsByClass('h1');
-var elements = document.getElementsByTagName('h1');
+$(function(){
+    $("#rollno").on('blur',function(){
+       if(!/(\d{2}((CO)|(DCO)|(EE)|(DEE)|(ME)|(dme)|(ce)|(dce)|(ces)|(dces))\d{2,3})/i.test(this.value)){
+           alert("INVALID ROLLNO");
+           this.value="";
+           $(this).focus();
+       } 
+    });
+    $("#sname").on('keypress',function(e){
+       if(/[^a-zA-Z ]/.test(e.key)){
+          alert("INVALID NAME. Only alphabets and spaces are allowed");
+           this.value="";
+           $(this).focus();
+           return false;
+       } 
+    });
+    $("#address").on('blur',function(){
+       if(!((this.value.length>=50)&&(this.value.length<=150))){
+          alert("INVALID ADRESS!!!");
+           this.value="";
+           $(this).focus();
+       } 
+    });
+});
